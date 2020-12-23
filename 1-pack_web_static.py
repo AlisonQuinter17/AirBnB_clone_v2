9220c8cdce98e2500342f73223e0c325fb61b299
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-12-22 21:08:46 -0500

    Compress before sending.

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Compress before sending."""
+from fabric.api import *
+import datetime
+from os import path
+
+
+def do_pack():
+    """Generates a .tgz archive."""
+    _datetime_ = datetime.now().strftime("%Y%m%d%H%M%S")
+    _path_ = "versions/web_static_" + _datetime_ + ".tgz"
+
+    if path.exists("versions") is False:
+        run("mkdir versions")
+    local("tar -zcvf {} web_static".format(_path_))
+    return _path_
+
+    else:
+        return None
