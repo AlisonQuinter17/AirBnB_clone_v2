a078e163c311aa5be9e10e88ac6df10f69520682
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2020-12-23 04:11:53 +0000

    Compress

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Compress before sending."""
+""" Compress before sending. """
 from fabric.api import *
-import datetime
+from datetime import datetime
 from os import path
 
 
 def do_pack():
-    """Generates a .tgz archive."""
-    _datetime_ = datetime.now().strftime("%Y%m%d%H%M%S")
-    _path_ = "versions/web_static_" + _datetime_ + ".tgz"
+    """do_pack method generates a .tgz archive."""
+    datime = datetime.now().strftime("%Y%m%d%H%M%S")
+    pth = "versions/web_static_" + datime + ".tgz"
 
-    if path.exists("versions") is False:
-        run("mkdir versions")
-    else:
+    try:
+        if path.exists("versions") is False:
+            run("mkdir versions")
+        local("tar -zcvf {} web_static".format(pth))
+        return pth
+    except:
         return None
-
-    local("tar -zcvf {} web_static".format(_path_))
-    return _path_
